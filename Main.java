55f54cb2c3113cb4c553102453e71f85ff65ec81
sanyaminkin <sanyaminkin@gmail.com>
2021-06-27 19:48:33 +0300

    .gitignore, Main.java and README.md added

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int balance = 200;
+        int refill = 1200;
+        int bonus = refill / 100;
+
+        int total;
+        if (refill > 1000) {
+            total = balance + refill + bonus;
+        } else {
+            total = balance + refill;
+        }
+
+        System.out.println("Баланс счета: " + total);
+
+
+    }
+}
